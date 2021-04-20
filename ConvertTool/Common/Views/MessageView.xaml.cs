0f9f10303cf5d820e10bb534ad52bf4446ace8ae
xuyachang <xuyachang@DESKTOP-IFPE335>
2021-04-20 19:03:21 +0800

    no message

---
+++ ConvertTool/Common/Views/MessageView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Common.Views
+{
+    /// <summary>
+    /// MessageView.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MessageView : Window
+    {
+        public MessageView()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
