07a18c5a5f086e61fefd61d6bae5d632305095f2
xuyachang <xuyachang@DESKTOP-IFPE335>
2021-04-28 15:04:19 +0800

    Add MyWindow

---
+++ ConvertTool/Common/Bases/MyWindow.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace Common.Bases
+{
+    public class MyWindow : Window
+    {
+        public static readonly DependencyProperty MaxButtonVisibilityProperty = DependencyProperty.Register(
+            "MaxButtonVisibility", typeof(Visibility), typeof(MyWindow), new PropertyMetadata(Visibility.Collapsed));
+
+        public Visibility MaxButtonVisibility
+        {
+            get => (Visibility)GetValue(MaxButtonVisibilityProperty);
+            set => SetValue(MaxButtonVisibilityProperty, value);
+        }
+
+
+    }
+}
