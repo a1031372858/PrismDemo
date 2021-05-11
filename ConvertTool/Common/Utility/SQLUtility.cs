e43cd5aac8b8d514cca66bd057e6a385bd643633
xuyachang <xuyachang@DESKTOP-IFPE335>
2021-05-11 16:13:27 +0800

    EntityFramkwork

--- ConvertTool/Common/Utility/SQLUtility.cs
+++ ConvertTool/Common/Utility/SQLUtility.cs
@@ -2,13 +2,13 @@
 
 namespace Common.Utility
 {
-    public class SQLUtility
+    public class SqlUtility
     {
         private const string DataSource = @"PORT=5432;DATABASE=usermanager;HOST=localhost;PASSWORD=666666;USER ID=postgres;";
 
         public static NpgsqlConnection Connection = new NpgsqlConnection(DataSource);
 
-        static SQLUtility()
+        static SqlUtility()
         {
 
             Connection.Open();
